669d32eac4a065ffbe3bad556eede58b5de82da0
Jef King <jefkin@microsoft.com>
2016-07-04 22:15:46 -0700

    client side

--- src/client/www/js/app.js
+++ src/client/www/js/app.js
@@ -6,19 +6,13 @@
 angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(cordova.platformId === 'ios' && window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+    $ionicPlatform.ready(function () {
+        var c = '#ffffff';
+        var letters = '0123456789ABCDEF'.split('');
+        var c = '#';
+        for (var i = 0; i < 6; i++) {
+            c += letters[Math.floor(Math.random() * 16)];
+        }
+        document.body.style.backgroundColor = c;
   });
 })
