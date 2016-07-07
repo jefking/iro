5e9cca194054542d50c8b3b8ee2e488fd17dfce6
Jef King <jefkin@microsoft.com>
2016-07-06 20:53:46 -0700

    hooked up

--- src/www/js/app.js
+++ src/www/js/app.js
@@ -4,7 +4,7 @@ var iroApp = angular.module('iroApp', ['ionic']);
 
 iroApp.controller('server', ['$scope', '$http', function ($scope, $http) {
     $http.get('http://iro.azurewebsites.net/').
-   success(function (data) {
-       $scope.payload = data;
-   });
+    success(function (data) {
+        $scope.payload = data;
+    });
 }]);
