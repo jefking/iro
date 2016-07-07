b75d8b894283a9587197dc576b9832a450ef7e48
Jef King <jefkin@microsoft.com>
2016-07-06 20:23:17 -0700

    misc

--- src/www/js/app.js
+++ src/www/js/app.js
@@ -1,22 +1,10 @@
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+'use strict';
 
-.run(function($ionicPlatform) {
-    $ionicPlatform.ready(function () {
-        setColor();
-        setInterval(setColor, 5000);
-  });
-})
+var iro = angular.module('iro', ['ionic']);
 
-function setColor()
-{
-    var c = '#ffffff';
-    var letters = '0123456789ABCDEF'.split('');
-    var c = '#';
-    for (var i = 0; i < 6; i++) {
-        c += letters[Math.floor(Math.random() * 16)];
-    }
-    document.body.style.backgroundColor = c;
-}
+iro.controller('server', ['$scope', '$http', function ($scope, $http) {
+    $http.get('http://iro.azurewebsites.net/').
+       success(function (data) {
+           $scope.payload = data;
+       });
+}]);
