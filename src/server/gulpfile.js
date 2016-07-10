a81326171bd421e7f7bef65d47b04599a8793e1d
Jef King <jefkin@microsoft.com>
2016-07-10 10:24:27 -0700

    gulp deployments

--- src/server/gulpfile.js
+++ src/server/gulpfile.js
@@ -1,5 +1,15 @@
+var zip = require("gulp-zip");
 var gulp = require('gulp');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task("deploy", function () {
+  return gulp.src(["**"])
+      .pipe(zip("deploy.zip"))
+      .pipe(gulp.dest("./"));
+});
+
+const fs = require('fs');
+
+gulp.task('compile', function () {
+  gulp.src(['**/*.ts', '!**/node_modules/**'])
+  .pipe(gulp.dest('./'))
 });
