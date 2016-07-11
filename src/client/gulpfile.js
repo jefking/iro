6f706db12268a6e35ac3b858f8e0197f293cccdc
Jef King <jefkin@microsoft.com>
2016-07-11 14:30:00 -0700

    remove zip

--- src/client/gulpfile.js
+++ src/client/gulpfile.js
@@ -1,12 +1,5 @@
-var zip = require("gulp-zip");
 var gulp = require('gulp');
 
 gulp.task('default', function() {
   // place code for your default task here
-});
-
-gulp.task("deploy", function () {
-  return gulp.src(["./www"])
-      .pipe(zip("deploy.zip"))
-      .pipe(gulp.dest("./"));
 });
